b017200f966513ddafb7e4990d576b741e0a16ab
Juliano Schroeder <juliano7s@gmail.com>
2014-10-13 23:30:26 -0300

    First commit. Basic mouse tracking for JumpyController

---
+++ Assets/Scripts/JumpyController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpyController : MonoBehaviour
+{
+	private Vector2 jumpCmdStart;
+	private Vector2 jumpCmdEnd;
+	private Vector2 jumpVector;
+	private bool mouseIsDown;
+
+    // Use this for initialization
+    void Start ()
+    {
+ 		mouseIsDown = false;
+    }
+
+    void Update ()
+    {    
+    	if (Input.GetMouseButtonDown(0))
+    	{
+			Ray mouseStart = Camera.main.ScreenPointToRay(Input.mousePosition);   		
+    		jumpCmdStart = new Vector2(mouseStart.origin.x, mouseStart.origin.y);
+			Debug.Log("Left click down at: " + jumpCmdStart);
+			mouseIsDown = true;
+    	}
+    	
+    	if (mouseIsDown)
+    	{
+			Ray mouseEnd = Camera.main.ScreenPointToRay(Input.mousePosition);   		
+			jumpCmdEnd = new Vector2(mouseEnd.origin.x, mouseEnd.origin.y);
+			jumpVector = new Vector2(Mathf.Abs(jumpCmdStart.x - jumpCmdEnd.x),
+				Mathf.Abs(jumpCmdStart.y - jumpCmdEnd.y));
+			if (jumpVector.x > 1 || jumpVector.y > 1)
+				Debug.Log("Jump cmd hold: deltaX = " + jumpVector.x + ", deltaY = " + jumpVector.y);			
+    	}
+    	
+		if (Input.GetMouseButtonUp(0))
+		{			
+			Debug.Log("Left click up at: " + jumpCmdEnd);			
+			Debug.Log("Jump cmd release: deltaX = " + jumpVector.x + ", deltaY = " + jumpVector.y);			
+			mouseIsDown = false;
+		} 
+    }
+}
---
+++ Assets/Scripts/SideDetector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideDetector : MonoBehaviour
+{
+    private Movement mScript;
+        
+    // Use this for initialization
+    void Start ()
+    {
+        mScript = transform.parent.GetComponent<Movement> ();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {        
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction);
+        
+        if (hit.collider != null && hit.collider == this.collider2D) {
+            //Direction of movement is opposite of collider detection
+            float direction = Mathf.Sign(collider2D.transform.position.x - transform.parent.position.x);
+            mScript.goRight = -direction;
+            //Debug.Log ("Mouse is over " + name + " - direction is " + direction);
+        }
+    }
+    
+}
